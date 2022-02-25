969df03951575b0c8dab6ea6b814ea7acd09c288
velco22 <velco22@gmail.com>
2022-02-25 14:57:23 +0100

    uprava

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ import logging
 import sys
 import socketserver
 
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', filename='proxy_call_log.log', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', filename='proxy_call_log.log', level=logging.INFO,
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
-hostname = socket.gethostname()
-ipaddress = socket.gethostbyname(hostname)
-if ipaddress == "127.0.0.1":
-    ipaddress = sys.argv[1]
+ipaddress = str()
+
+q = input("Pre ziskanie IP adresy pre server zadajte Y pre zadanie IP adresy nieco zadajte: ")
+
+if q == 'Y' or q == 'y':
+    hostname = socket.gethostname()
+    ipaddress = socket.gethostbyname(hostname)
+    if ipaddress == "127.0.0.1":
+        ipaddress = sys.argv[1]
+else:
+    ipaddress = input("Zadajte IP adresu servera: ")
 
 print('IP Adresa SIP servera: ' + ipaddress)
 
